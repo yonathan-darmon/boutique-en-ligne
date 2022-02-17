a5b85db382273c1ef621a6c0048e1db9fcdfdb4f
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-02-17 17:16:14 +0100

    test de lien

--- view/produits.php
+++ view/produits.php
@@ -1,12 +1,12 @@
 <div class="box1">
-    <div class="categorie"><a href="produits.php?cat=disney">Disney</a></div>
-    <div class="categorie"><a href="produits.php?cat=hp">Harry Potter</a></div>
+    <div class="categorie"><a href=<?=ROOT."produits/disney"?>>Disney</a></div>
+    <div class="categorie"><a href="./harry_potter">Harry Potter</a></div>
     <div class="categorie"><a href="produits.php?cat=marvel">Marvel</a></div>
     <div class="categorie"><a href="produits.php?cat=sw">Star Wars</a></div>
     <div class="categorie"><a href="produits.php?cat=dc">DC Universe</a></div>
 </div>
 <h1>Liste des produits</h1>
-
+<?php var_dump($produits);?>
 <?php foreach($produits as $value):?>
     <h2><a href="produit.php?<?=$value['id']?>"><?= $value['name'];?></a></h2>
 <h3><?= $value['price'];?> euros</h3>
