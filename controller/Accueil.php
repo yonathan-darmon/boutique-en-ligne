07e32ee84d94f301aca3afa04ef4b618c219d99a
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2022-02-24 14:05:34 +0100

    accueil commencé

---
+++ controller/Accueil.php
@@ -0,0 +1,8 @@
+<?php
+    class Accueil extends Controller
+    {
+        public static function index(){
+            self::render('accueil');
+        }
+    }
+?>
--- view/accueil.php
+++ view/accueil.php
@@ -1,5 +1,37 @@
 <?php
 
 ?>
-<h1>coucou</h1>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="page-accueil.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+    integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Accueil</title>
+</head>
+<body>
+<main>
+    <div class="wrapper">
+      <div class="fixed one"></div>
+      <div class="scroll">
+        <h3>
+          "Dans la vie on ne fait pas ce que l'on veut mais on est responsable de ce que l'on est.."
+        </h3>
+      </div>
+      <div class="fixed two"></div>
+      <div class="scroll">
+        <h3>
+          "On passe une moitié de sa vie à attendre ceux qu'on aimera et l'autre moitié à quitter ceux qu'on aime.."
+        </h3>
+      </div>
+      <div class="fixed three"></div>
+    </div>
+    </main>
+</body>
+</html>
 
