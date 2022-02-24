646d319469de50939669482dfc37c985a774f0f6
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-02-24 09:34:54 +0100

    Update produits.php

--- view/produits.php
+++ view/produits.php
@@ -6,7 +6,6 @@
     <div class="categorie"><a href="produits.php?cat=dc">DC Universe</a></div>
 </div>
 <h1>Liste des produits</h1>
-<?php var_dump($produits);?>
 <?php foreach($produits as $value):?>
     <h2><a href="produit.php?<?=$value['id']?>"><?= $value['name'];?></a></h2>
 <h3><?= $value['price'];?> euros</h3>
