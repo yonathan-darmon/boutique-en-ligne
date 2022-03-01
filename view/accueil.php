eeacb5f47374f665f8055f68c2710214067bf5ee
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2022-03-01 09:18:33 +0100

    back page accueil en cours

--- view/accueil.php
+++ view/accueil.php
@@ -6,11 +6,41 @@
     <h1>Produits en promotion !</h1>
     <?php foreach ($produit as $value) :?>
         <p><?=$value['name'];?></p>
-        <p><?=$value['price'];?>€</p>
         <?=$price = $value['price'];?>
-        <?=$reduc = ($value['price'] * $value['promo']) /100;?>
-        <? $priceRed = ($price - $reduc)?>
+        <?php $reduc = ($value['price'] * $value['promo']) /100;?>
+        <?=$priceRed = ($price - $reduc)?>
 <?php endforeach;?>
+</div>
+
+<div class="bloc1">
+      <h1>Nos produits phares !</h1>
+      <?php foreach($produit as $value):?>
+      <?php if($value['mis_avant'] == 1) 
+      {
+        echo $value['name'];
+        echo $value['price']. '€'; 
+    }
+      ?>
+      <?php endforeach;?>
     </div>
 
+    <div class="bloc2">
+    <h1>Nos top ventes !</h1>
+    <?php foreach($produit as $value):?>
+        <?php if($value['stock'] <= 3) 
+      {
+        echo $value['name'];
+        echo $value['price']. '€'; 
+        echo $value['stock']. 'pce';
+    }
+      ?>
+      <?php endforeach;?>
+    </div>
 
+    <div class="bloc3">
+    <h1>Nos derniers arrivages</h1>
+    <?php foreach($produit2 as $value):?>
+        <p><?=$value['name'];?></p>
+        <p><?=$value['price'];?></p>
+        <?php endforeach;?>
+    </div>
