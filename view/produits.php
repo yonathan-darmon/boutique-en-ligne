4688b08b6d767f10c0b0635f367bb89ea52c870a
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-07 12:28:40 +0100

    modif de systeme de panier

--- view/produits.php
+++ view/produits.php
@@ -1,4 +1,4 @@
-<div class="box1">
+<div class="box1" xmlns="http://www.w3.org/1999/html">
     <div class="categorie"><a href="<?= path ?>produits/disney">Disney</a></div>
     <div class="categorie"><a href="<?= path ?>produits/harry_potter">Harry Potter</a></div>
     <div class="categorie"><a href="<?= path ?>produits/marvel">Marvel</a></div>
@@ -8,7 +8,9 @@
 <h1>Découvrez notre collection</h1>
 <div class="box">
     <div class="research">
-        <form action="<?= path ?>produits/<?php if(isset($_POST['filtre'])) {echo $_POST['filtre'];}?>" name="select" method="post">
+        <form action="<?= path ?>produits/<?php if (isset($_POST['filtre'])) {
+            echo $_POST['filtre'];
+        } ?>" name="select" method="post">
             <select name="filtre" id="filtre">
                 <?php foreach ($categorie as $value): ?>
                     <option value="<?= $value['name_categories']; ?>"><?= $value['name_categories']; ?></option>
@@ -18,18 +20,32 @@
         </form>
 
     </div>
+    <?php if(!isset($produit)):?>
     <div class="productlist">
         <?php foreach ($produits as $value): ?>
             <h2><a href="<?= path ?>article/<?= $value['id'] ?>"><?= $value['name']; ?></a></h2>
             <h3><?= $value['price']; ?> euros</h3>
             <form action="#" method="post" name="pan">
-            <button name="panier" value="achat<?=$value['id']?>">Achetez</button>
+                <input type="hidden" name="hidden" value="<?= $value['id'] ?>">
+                <input type="submit" name="achat" value="acheter">
             </form>
         <?php endforeach; ?>
 
     </div>
+    <?php endif;?>
+    <?php if(isset($produit)):?>
+    <div class="popup">
+        <h1>Vous avez ajouter ce produit au panier</h1>
+        <h2><?=$produit[0]['name']?></h2>
+        <a href="<?=path?>panier">Aller au panier</a>
+        <a href="<?=path?>produits">Continuer vos achats</a>
+    </div>
+    <?php endif;?>
+
 </div>
 
-<?php for($i=1; $i<=$pages; $i++): ?>
-        <a href="<?= path ?>produits"><?= $i ?></a>&nbsp
-<?php endfor;?>
+
+<?php
+for ($i = 1; $i <= $pages; $i++): ?>
+    <a href="<?= path ?>produits"><?= $i ?></a>&nbsp
+<?php endfor; ?>
