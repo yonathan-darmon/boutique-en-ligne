2aafd48bcd49b6db3aefe6c446c5d1a3218120e5
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-07 17:20:59 +0100

    maj de la redirection de connexion

--- controller/Controller.php
+++ controller/Controller.php
@@ -28,68 +28,68 @@ class Controller
 
     public static function sendmail()
     {
-        if(isset($_POST['send'])) {
+        if (isset($_POST['send'])) {
 
-            
+
             //Create a new PHPMailer instance
             $mail = new PHPMailer();
-            
+
             //Tell PHPMailer to use SMTP
             $mail->isSMTP();
-            
+
             //Enable SMTP debugging
             //SMTP::DEBUG_OFF = off (for production use)
             //SMTP::DEBUG_CLIENT = client messages
             //SMTP::DEBUG_SERVER = client and server messages
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-            
+
             //Set the hostname of the mail server
-            $mail -> Mailer = 'smtp';
+            $mail->Mailer = 'smtp';
             $mail->Host = 'smtp.gmail.com';
             //Use `$mail->Host = gethostbyname('smtp.gmail.com');`
             //if your network does not support SMTP over IPv6,
             //though this may cause issues with TLS
-            
+
             //Set the SMTP port number: 587 for SMTP+STARTTLS
             $mail->Port = 587;
-            
+
             //Set the encryption mechanism to use: STARTTLS (explicit TLS on port 587)
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            
+
             //Whether to use SMTP authentication
             $mail->SMTPAuth = true;
-            $mail->SMTPSecure = 'tls';  
-            
+            $mail->SMTPSecure = 'tls';
+
             //Username to use for SMTP authentication - use full email address for gmail
             $mail->Username = 'aurelien.adjimi@laplateforme.io';
-            
+
             //Password to use for SMTP authentication
             $mail->Password = 'Ekqd9m9q2asi';
-            
+
             //Set who the message is to be sent from
             $mail->setFrom($_POST['mail'], $_POST['nom']);
-            
+
             //Set an alternative reply-to address
             //$mail->addReplyTo('replyto@example.com', 'First Last');
-            
+
             //Set who the message is to be sent to
             $mail->addAddress('aurelien.adjimi@laplateforme.io', 'Aurélien Adjimi');
-            
+
             //Set the subject line
             $mail->Subject = $_POST['objet'];
-            
+
             //Read an HTML message body from an external file, convert referenced images to embedded,
             //convert HTML into a basic plain-text alternative body
             //$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-            
+
             //Config body mail
-            $mail -> WordWrap = 70;
-            $mail -> CharSet = 'utf-8';
+            $mail->WordWrap = 70;
+            $mail->CharSet = 'utf-8';
             $mail->Body = $_POST['message'];
-            
+
             //Attach an image file
             //$mail->addAttachment('images/phpmailer_mini.png');
-            
+
             //send the message, check for errors
             if (!$mail->send()) {
                 echo 'Oups, essayez de nouveau !' . $mail->ErrorInfo;
@@ -103,13 +103,14 @@ class Controller
             }
         }
     }
+
     public function disconnect($id)
     {
-        if (isset($_POST['deco'])){
-            $panier=new PanierModel();
+        if (isset($_POST['deco'])) {
+            $panier = new PanierModel();
             $panier->delete($_SESSION['id']);
             unset($_SESSION['id']);
-            header('location:'.path.'accueil');
+            header('location:' . path . 'accueil');
         }
     }
 }
