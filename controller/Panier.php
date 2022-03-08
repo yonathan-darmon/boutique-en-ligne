0f31343c65f181db7f592bed706aa1d407a5ddad
thomas-germany <thomas.germany@laplateforme.io>
2022-03-08 14:39:33 +0100

    panier

--- controller/Panier.php
+++ controller/Panier.php
@@ -3,8 +3,14 @@
     class Panier extends Controller
     {
         public static function index()
-        {
-            self::render('panier');
+        {         
+            if(isset($_SESSION['id'])){
+                $model = new paniermodel();
+                $panier = $model->getAll();
+                //var_dump($panier);
+                //$panier = $model->getInnerJoin('products', 'id_products', 'id', 'id_user');
+                self::render('panier', compact('panier'));
+            }
         }
     }
 ?>
--- view/panier.php
+++ view/panier.php
@@ -1,3 +1,14 @@
-<?php
+<?php foreach($panier as $value):?>
+    <?php //var_dump($panier) ?>
+    <h3>Prix</h3>
+    <p><?=$value['price'];?>€</p>
+    <h3>Quantité</h3>
+    <p><?=$value['quantity'];?></p>
+    <h3>Total</h3>
+    <p><?=$value['price']*$value['quantity'];?>€</p>
+<?php endforeach;?>
 
-?>
+<div class="paiement">
+    <h2>Total panier</h2>
+    <p>Total: <?=$value['price']*$value['quantity'];?>€</p>
+</div>
