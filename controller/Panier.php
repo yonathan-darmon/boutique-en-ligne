33aa1445fc37535c7be36064412762a7b47f78bb
thomas-germany <thomas.germany@laplateforme.io>
2022-03-10 14:57:06 +0100

    stripe

--- controller/Panier.php
+++ controller/Panier.php
@@ -7,10 +7,19 @@
             if(isset($_SESSION['id'])){
                 $model = new paniermodel();
                 $panier = $model->getAll();
-                //var_dump($panier);
+                var_dump($panier);
                 //$panier = $model->getInnerJoin('products', 'id_products', 'id', 'id_user');
-                self::render('panier', compact('panier'));
             }
+
+            if(isset($_POST['supprimer'])){
+                $panier = $model->delete($_SESSION['id']);
+                header('Refresh:2,' . path . 'panier');
+            }
+            if(isset($_POST['modifquantite'])){
+                $quantity = $_POST['quantity'];
+                $panier = $model->update('quantity', $quantity, 'id_product');
+            }
+            self::render('panier', compact('panier'));
         }
     }
 ?>
--- view/panier.php
+++ view/panier.php
@@ -1,14 +1,48 @@
-<?php foreach($panier as $value):?>
-    <?php //var_dump($panier) ?>
-    <h3>Prix</h3>
-    <p><?=$value['price'];?>€</p>
-    <h3>Quantité</h3>
-    <p><?=$value['quantity'];?></p>
-    <h3>Total</h3>
-    <p><?=$value['price']*$value['quantity'];?>€</p>
-<?php endforeach;?>
+<div class="panier">
+    <?php
+        if(empty($panier)){
+            echo '<p>votre panier est vide</p>';
+        }
+    ?>
+
+    <?php foreach($panier as $value):?>
+        <?php //var_dump($panier) ?>
+        <h3>Prix</h3>
+        <p><?=$value['price'];?>€</p>
+        <h3>Quantité</h3>
+        <input type="number" name="modifquantite" value="<?=$value['quantity'];?>">
+        <h3>Total</h3>
+        <p><?=$value['price']*$value['quantity'];?>€</p>
+        <form method="post">
+            <input type="submit" name="supprimer" value="supprimer">
+        </form>
+    <?php endforeach;?>
+</div>
 
 <div class="paiement">
-    <h2>Total panier</h2>
-    <p>Total: <?=$value['price']*$value['quantity'];?>€</p>
+        <h2>Total panier</h2>
+        <p>Total: <?=$value['price']*$value['quantity'];?>€</p>
+       <?php
+            if(!empty($panier['price'])){
+                require_once('vendor/autload.php');
+                $prix = $panier['price'];
+                //on instancie stripe
+                \Stripe\Stripe::setApiKey('sk_test_51Kb39iC5Di6WbNI4XF9KPchnOZOxF0x8XIIADxhzlGVgAoBL7oL9T0GUsyOO2fDaVWhOqDFjeo1bxHDuHc27XYP700BKvleeR3');
+                $PaymentIntent = \Stripe\PaymentIntent::create([
+                    'amount' => $prix*100,
+                    'currency' => 'eur'
+                ]);
+            }
+       ?>
 </div>
+<body>
+    <form method="post">
+        <div id="errors"></div><!--messages d'erreur de paiement-->
+        <input type="text" id="cardholder-name" placeholder="Titulaire de la carte">
+        <div id="card-elements"></div><!--formulaire des informations de la carte-->
+        <div id="card-errors" role="alert"></div><!--erreur pour la carte-->
+        <button id="card-button" type="button">Procéder au paiement</button>
+    </form>
+
+    
+</body>
