4488957c06b509b6c3a69370ab25f16373c8e71e
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-10 16:05:13 +0100

    mise en page

--- view/profil.php
+++ view/profil.php
@@ -1,23 +1,26 @@
-<?php foreach ($utilisateur as $value): ?>
-<div class="bigboite">
-    <div class="boite1">
-        <div class="value">
-            <p><?= $value['login'] ?> </p>
-            <p> Votre mot de passe</p>
-            <p><?= $value['adresse'] ?></p>
-            <p><?= $value['email'] ?></p>
-        </div>
-        <?php endforeach; ?>
+<?php foreach ($utilisateur
 
-        <div class="link">
-            <a href="<?= path ?>profil/login">Modifiez votre login</a>
-            <a href="<?= path ?>profil/password">Modifiez votre mot de passe</a>
-            <a href="<?= path ?>profil/adresse">Modifiez votre adresse</a>
-            <a href="<?= path ?>profil/email">Modifiez votre emaiil</a>
-        </div>
+as $value): ?>
+<div class="boite1">
+    <div class="value">
+        <p><?= $value['login'] ?> </p>
+        <p> Votre mot de passe</p>
+        <p><?= $value['adresse'] ?></p>
+        <p><?= $value['email'] ?></p>
     </div>
-    <a href="<?= path ?>profil/historique_des_commandes">Historique des commandes</a>
+    <?php endforeach; ?>
 
+    <div class="link">
+        <a href="<?= path ?>profil/login">Modifiez votre login</a>
+        <a href="<?= path ?>profil/password">Modifiez votre mot de passe</a>
+        <a href="<?= path ?>profil/adresse">Modifiez votre adresse</a>
+        <a href="<?= path ?>profil/email">Modifiez votre emaiil</a>
+        <a href="<?= path ?>profil/historique_des_commandes">Historique des commandes</a>
+
+    </div>
+
+</div>
+<div class="reward">
     <h2>Votre niveau de reward</h2>
-    <h1><?= $reward['name']; ?></h1>
+    <h1 class="metal"><?= $reward['name']; ?></h1>
 </div>
