2f0958637b6fae27b289f9b2901d87b43567deb2
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-11 13:33:34 +0100

    update

--- controller/Admin.php
+++ controller/Admin.php
@@ -1,74 +1,75 @@
 <?php
 
-class Admin extends Controller 
+class Admin extends Controller
 {
     public function __construct()
     {
-        
+
     }
 
     public static function index()
     {
-       $user=new UtilisateursModel();
-       $utilisateur=count($user->getALL());
-       $product=new HistoriqueModel();
-       $produit=count($product->getALL());
-       $stock=new ProduitsModel();
-       $stocklow=$stock->getLowStock();
-        self::renderAdmin('admin',compact('utilisateur','produit','stocklow'));
+        $user = new UtilisateursModel();
+        $utilisateur = count($user->getALL());
+        $product = new HistoriqueModel();
+        $produit = count($product->getALL());
+        $stock = new ProduitsModel();
+        $stocklow = $stock->getLowStock();
+        self::renderAdmin('admin', compact('utilisateur', 'produit', 'stocklow'));
     }
 
-    public static function stock ()
+    public static function stock()
     {
         $stock = new ProduitsModel();
         $rendstock = ($stock->stock());
         //$addstock = $stock->update('stock', );
         self::renderAdmin('adminstock', compact('rendstock'));
     }
-    public static function adStock($params)
+
+    public static function adStock()
     {
+        $params=explode('/',$_GET['p']);
         $stock = new ProduitsModel();
-        $addstock = ($stock ->getOne('id', $params));
-        self::renderAdmin('adminadstock', compact('addstock'));
+        $addstock = ($stock->getOne('id', $params[2]));
+        if (isset($_POST['ad'])) {
+            $stock->upstock($_POST['num'], $params[2]);
+            header('location:' . path . 'admin');
+        } else {
+            self::renderadmin('adminadstock', compact('addstock'));
+
+        }
     }
 
-    public static function upstock()
+    public static function user()
     {
-        $stock = new ProduitsModel();
-        $upstock = ($stock->upstock($params $value));
-        self::renderAdmin('adminadstock', compact('upstock'));
-    }
-
-    public static function user ()
-    {
-        $user=new UtilisateursModel();
-        $utilisateur=($user->getALL());
+        $user = new UtilisateursModel();
+        $utilisateur = ($user->getALL());
         self::renderAdmin('adminuser', compact('utilisateur'));
     }
 
-    public static function articles ()
+    public static function articles()
     {
         $nom =
         $prix =
-        $stock = 
-        $promo = 
-        $push = 
-        $short = 
-        $long = 
-        $tags = 
+        $stock =
+        $promo =
+        $push =
+        $short =
+        $long =
+        $tags =
         $add = new ProduitsModel();
         $addprod = $add->addProd($nom, $prix, $stock, $promo, $push, $short, $long, $tags);
         self::renderAdmin('adminarticles', compact('addprod'));
     }
 
-    public static function ventes ()
+    public static function ventes()
     {
-        $product=new HistoriqueModel();
-        $produit=($product->getALL());
+        $product = new HistoriqueModel();
+        $produit = ($product->getALL());
         self::renderAdmin('adminventes', compact('produit'));
     }
 
-    public static function categories ()
+    public static function categories()
     {
         self::renderAdmin('admincategories');
     }
--- view/adminadstock.php
+++ view/adminadstock.php
@@ -1,17 +1,10 @@
 
-<?php foreach($addstock as $value)?>
-<?=$value['id']?>
-<?=$value['name']?>
-<?=$value['stock']?>
-
-<?php if(isset($addstock)):?>
-<?php if(isset($_POST['ad'])){
-
-
-
-    }?>
-<?php if(isset($upstock))?>
-<?php endif;?>
+<?php
+foreach($addstock as $value):?>
+<p><?=$value['id']?></p>
+<h1><?=$value['name']?></h1>
+<h2><?=$value['stock']?></h2>
+<?php endforeach;?>
 <form action="" method="POST">
 <input type="number" name="num"></input>
 <input type="submit" name="ad">
