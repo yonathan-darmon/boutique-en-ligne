0fc72e6eef7ac67018fb3a9e0d8c918ef7754447
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-11 15:32:29 +0100

    mise en page

--- view/article.php
+++ view/article.php
@@ -1,63 +1,63 @@
-
-<?php foreach($produit as $value):?>
-    <p><?=$value['name'];?></p>
-    <p><?=$value['price'];?>€</p>
-<?php endforeach;?>
+<?php foreach ($produit as $value): ?>
+    <img src="<?=path?>/ASSET/images/<?= $value['image'] ?>" alt="">
+    <p><?= $value['name']; ?></p>
+    <p><?= $value['price']; ?>€</p>
+<?php endforeach; ?>
 
 <div>
     <p>Note moyenne des utilisateurs</p>
-    <a href="#commentaire"> 
+    <a href="#commentaire">
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
     </a>
-    <?php foreach($commentsaverage as $value): ?>
+    <?php foreach ($commentsaverage as $value): ?>
         <?php
-            $value = implode(',', $value);
-            echo $value;
+        $value = implode(',', $value);
+        echo $value;
         ?>
-    <?php endforeach;?>
+    <?php endforeach; ?>
 </div>
 
 <div class="bouttonpanier">
     <form action="#" method="post">
         <i class="fa-solid fa-basket-shopping"></i>
-        <input type="submit" value="Ajouter au panier" name="panier" >
+        <input type="submit" value="Ajouter au panier" name="panier">
     </form>
 </div>
 
 <h1>Descriptifs</h1>
 
-<?php foreach($produit as $value): ?>
-    <p><?=$value['short_descr'];?></p>
-    <p><?=$value['long_descr'];?></p>
-<?php endforeach;?>
+<?php foreach ($produit as $value): ?>
+    <p><?= $value['short_descr']; ?></p>
+    <p><?= $value['long_descr']; ?></p>
+<?php endforeach; ?>
 
 <h1>Commentaires</h1>
 <div id="commentaire">
     <div class="commentaire">
         <?php
-            if(empty($comments)){
-                echo '<p>Soyez le premier à donner votre avis</p>';
-            }
+        if (empty($comments)) {
+            echo '<p>Soyez le premier à donner votre avis</p>';
+        }
         ?>
 
-        <?php foreach($comments as $value): ?>
-            <p><?=$value['comment'];?></p>
-            <p> Le <?=$value['date'];?></p>
-            <p> Par <?=$value['login'];?></p>
-            <p><?=$value['approuval'];?> étoile(s)</p>
+        <?php foreach ($comments as $value): ?>
+            <p><?= $value['comment']; ?></p>
+            <p> Le <?= $value['date']; ?></p>
+            <p> Par <?= $value['login']; ?></p>
+            <p><?= $value['approuval']; ?> étoile(s)</p>
             <hr>
-        <?php endforeach;?>
+        <?php endforeach; ?>
     </div>
 </div>
 
 <h2>Laisser un commentaire</h2>
 <?php
-    if(isset($_SESSION['id'])){
-        echo '<form action="#" method="post">
+if (isset($_SESSION['id'])) {
+    echo '<form action="#" method="post">
                 <textarea name="commentaire" rows="5" cols="33" placeholder="Laisser un message..."></textarea>
                 <br>
                 <p>Laisser nous une note</p>
@@ -72,8 +72,8 @@
                 <br></br>
                 <input type="submit" value="Envoyer" name="valider">
             </form>';
-    }
-    /*else{ ?>
-        <a href="<?=path?>connexion"><p>Connecter vous pour laisser un commentaire</p></a>
-    <?php }*/
+}
+/*else{ ?>
+    <a href="<?=path?>connexion"><p>Connecter vous pour laisser un commentaire</p></a>
+<?php }*/
 ?>
