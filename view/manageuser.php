d00dc217e25898758cd6e23653b42bfc605b1b5a
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2022-03-14 09:57:09 +0100

    Update manageuser

--- view/manageuser.php
+++ view/manageuser.php
@@ -1,7 +1,10 @@
-<?php
-var_dump($utilis);
-var_dump($droit);
-?>
+<?php foreach($utilis as $value)?>
+<p><?=$value['id']?></p>
+<h1><?=$value['login']?></h1>
+<h2><?=$value['id_droit']?></h2>
+<!-- var_dump($utilis);
+var_dump($droit); -->
+
 <form action="#" method="post" name="droit">
     <input type="text" name="droit">
     <input type="submit" name="modif">
