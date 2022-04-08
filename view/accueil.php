63836e0da881e7b0a7cbfbbd667707c07340bfc3
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-04-08 11:26:57 +0200

    mise en page accueil

--- view/accueil.php
+++ view/accueil.php
@@ -24,57 +24,68 @@
 
 <div class="bloc">
     <h1>Produits en promotion !</h1>
-        <?php foreach ($promo as $value) :?>
-          <div class="carte">
-        <img src="<?= path ?>ASSET/images/<?=$value['image2']?>" alt=""><br>
-        <h3><?=$value['name'];?></h3><br>
-        <p>Prix:<?=$price = $value['price']?>€<br></p><br>
-        <?php $reduc = ($value['price'] * $value['promo']) /100;?>
-        <p>Prix réduit:<?= $priceRed = ($price - $reduc). '€';?></p>
+    <div class="sousbloc">
+        <?php foreach ($promo as $value) : ?>
+            <div class="carte">
+                <div class="cardimage">
+
+                    <img src="<?= path ?>ASSET/images/<?= $value['image2'] ?>" alt=""><br>
+                </div>
+                <div class="cardcontent">
+
+                    <h3><?= $value['name']; ?></h3><br>
+                    <p>Prix:<?= $price = $value['price'] ?>€<br></p><br>
+                    <?php $reduc = ($value['price'] * $value['promo']) / 100; ?>
+                    <p>Prix réduit:<?= $priceRed = ($price - $reduc) . '€'; ?></p>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
     </div>
-    <?php endforeach;?>
 </div>
 <div class="parallax-effect"></div>
 
 <div class="bloc1">
     <h1>Nos produits phares !</h1>
-    <?php foreach($avant as $value):?>
-    <div class="carte">
-        <img src="<?= path ?>ASSET/images/<?=$value['image2']?>" alt=""><br>
-        <h3><?=$value['name'];?></h3><br>
-        <p>Prix:<?=$price = $value['price']. '€'?></p><br><br>
-        <?php $reduc = ($value['price'] * $value['promo']) /100;?>
-        <?php 
-        if($value['promo'] != 0 && $value['mis_avant'] == 1) 
-        {
-          $price = $value['price'];
-          $reduc = ($value['price'] * $value['promo']) /100;
-          $priceRed = ($price - $reduc). '€';
-          echo '<p> Prix réduit:'.$priceRed. '</p>';
-        }
-        ?>
-    </div>
-    <?php endforeach;?>
+    <?php foreach ($avant as $value): ?>
+        <div class="carte">
+            <div class="cardimage">
+                <img src="<?= path ?>ASSET/images/<?= $value['image2'] ?>" alt=""><br>
+            </div>
+            <div class="cardcontent">
+                <h3><?= $value['name']; ?></h3><br>
+                <p>Prix:<?= $price = $value['price'] . '€' ?></p><br><br>
+                <?php $reduc = ($value['price'] * $value['promo']) / 100; ?>
+                <?php
+                if ($value['promo'] != 0 && $value['mis_avant'] == 1) {
+                    $price = $value['price'];
+                    $reduc = ($value['price'] * $value['promo']) / 100;
+                    $priceRed = ($price - $reduc) . '€';
+                    echo '<p> Prix réduit:' . $priceRed . '</p>';
+                }
+                ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
 <div class="parallax-effect1"></div>
 
 <div class="bloc2">
     <h1>Nos derniers arrivages</h1>
-    <?php foreach($produit as $value):?>
-    <div class="carte">
-        <img src="<?= path ?>ASSET/images/<?=$value['image2']?>" alt=""><br>
-        <h3><?=$value['name'];?></h3><br>
-        <p>Prix:<?=$value['price']. '€';?></p><br><br>
-        <?php 
-        if($value['promo'] != 0) 
-        {
-          $price = $value['price'];
-          $reduc = ($value['price'] * $value['promo']) /100;
-          $priceRed = ($price - $reduc);
-          echo '<p> Prix réduit:'.$priceRed. '</p>';
-        }
-        ?>
-    </div>
-    <?php endforeach;?>
+    <?php foreach ($produit as $value): ?>
+        <div class="carte">
+            <img src="<?= path ?>ASSET/images/<?= $value['image2'] ?>" alt=""><br>
+            <h3><?= $value['name']; ?></h3><br>
+            <p>Prix:<?= $value['price'] . '€'; ?></p><br><br>
+            <?php
+            if ($value['promo'] != 0) {
+                $price = $value['price'];
+                $reduc = ($value['price'] * $value['promo']) / 100;
+                $priceRed = ($price - $reduc);
+                echo '<p> Prix réduit:' . $priceRed . '</p>';
+            }
+            ?>
+        </div>
+    <?php endforeach; ?>
 </div>
 <div class="parallax-effect2">
