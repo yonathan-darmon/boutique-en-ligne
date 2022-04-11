5bb0f1ce272fc5372568a720e0e2e224a423bb87
aurelien-adjimi <91850318+aurelien-adjimi@users.noreply.github.com>
2022-04-11 14:44:38 +0200

    bug corrigé

--- model/SouscategorieModel.php
+++ model/SouscategorieModel.php
@@ -8,7 +8,7 @@ class SouscategorieModel extends Model{
 
     public function addSousCat($nameSC,$idcat) // Requète pour ajouter une sous catégorie
     {
-        $sth = $this->_connexion->prepare('INSERT INTO sous_categories (name, id_categories) VALUES (?, ?)');
+        $sth = $this->_connexion->prepare('INSERT INTO sous_categories (name, id_categorie) VALUES (?, ?)');
         $sth -> execute(array($nameSC,$idcat));
     }
 }
